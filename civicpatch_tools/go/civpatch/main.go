b5c46fecbaf5bf406b578826c09bcdce9af692c3
michelle@shelltr.me <michelle@shelltr.me>
2025-05-15 19:28:14 -0700

    WIP -- add back

--- civicpatch_tools/go/civpatch/main.go
+++ civicpatch_tools/go/civpatch/main.go
@@ -1,142 +1,57 @@
 package main
 
 import (
-	"bufio"
+	"civpatch/commands"
 	"context"
 	"flag"
 	"fmt"
 	"os"
-
-	"civpatch/docker"
-	"civpatch/services"
+	"strings"
 )
 
 var (
-	GITHUB_CLIENT_ID = "Iv23liEGI2vDgAHmft8C"
-	scrapeCommand    = flag.NewFlagSet("scrape", flag.ExitOnError)
-	withCi           = scrapeCommand.Bool("with-ci", false, "Run with CI")
-	createPr         = scrapeCommand.Bool("create-pr", false, "Create a PR")
-	develop          = scrapeCommand.Bool("develop", false, "Develop locally")
-	state            = scrapeCommand.String("state", "", "State to scrape")
-	gnis             = scrapeCommand.String("gnis", "", "GNIS ID to scrape")
+	scrapeCommand = flag.NewFlagSet("scrape", flag.ExitOnError)
+	state         = scrapeCommand.String("state", "", "State to scrape")
+
+	scrapePlan   = scrapeCommand.Bool("plan", false, "Plan the scrapes")
+	numScrapes   = scrapeCommand.Int("num-scrapes", 1, "Number of scrapes to plan")
+	gnisToIgnore = scrapeCommand.String("gnis-to-ignore", "", "GNIS IDs to ignore")
+
+	scrapeRun = scrapeCommand.Bool("run", false, "Run the scrapes")
+	createPr  = scrapeCommand.Bool("create-pr", false, "Create a PR")
+	develop   = scrapeCommand.Bool("develop", false, "Develop locally")
+	gnis      = scrapeCommand.String("gnis", "", "GNIS ID to scrape")
+	withCi    = scrapeCommand.Bool("with-ci", false, "Run with CI")
+
+	// authClear = flag.NewFlagSet("auth-clear", flag.ExitOnError)
 	// geoid         = scrapeCommand.String("geoid", "", "GEOID to scrape") TODO: FIX
-	municipalities = flag.NewFlagSet("municipalities", flag.ExitOnError)
 )
 
-func checkGitHubCredentials(ctx context.Context) string {
-	deviceFlow, err := services.NewGitHubDeviceFlow(GITHUB_CLIENT_ID)
-	if err != nil {
-		fmt.Println("Error creating device flow:", err)
-		os.Exit(1)
+func scrapeCommands(ctx context.Context, scrapePlan bool, scrapeRun bool) error {
+	var output string
+	var err error
+
+	if scrapePlan {
+		output, err = commands.ScrapePlan(*state, *numScrapes, strings.Split(*gnisToIgnore, ","))
+		if err != nil {
+			return err
+		}
 	}
-
-	client, err := deviceFlow.NewClient(ctx)
-	if err != nil {
-		fmt.Println("Error getting GitHub Client", err)
-		os.Exit(1)
-	}
-
-	user, _, err := client.Users.Get(ctx, "")
-	if err != nil {
-		fmt.Println("Error getting user:", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Successfully authenticated as %s\n", *user.Name)
-
-	github_token, err := deviceFlow.GetToken(ctx)
-	if err != nil {
-		fmt.Println("Error getting GitHub token:", err)
-		os.Exit(1)
-	}
-
-	return github_token.AccessToken
-}
-
-func checkScrapeInput(state string, gnis string, withCI bool) {
-	if withCI {
-		checkEnvironmentVariables(requiredEnvVarsCI)
-	} else {
-		checkEnvironmentVariables(requiredEnvVars)
-	}
-
-	if state == "" {
-		fmt.Println("Error: state is required")
-		os.Exit(1)
-	}
-	if gnis == "" {
-		fmt.Println("Error: gnis is required")
-		os.Exit(1)
-	}
-}
-
-func scrape(state string, gnis string, createPr bool, withCi bool, develop bool) error {
-	ctx := context.Background()
-	checkScrapeInput(state, gnis, withCi)
-
-	github_token := os.Getenv("GITHUB_TOKEN")
-	if !withCi {
-		github_token = checkGitHubCredentials(ctx)
-	}
-
-	fmt.Printf("Scraping with createPr: %t\n", createPr)
-
-	dockerClient, err := docker.NewClient()
-	if err != nil {
-		fmt.Println("Error creating docker client:", err)
-		os.Exit(1)
-	}
-	defer dockerClient.Close()
-
-	imageTag := "civpatch"
-	dockerfile := "Dockerfile.civpatch"
-	if develop {
-		imageTag = "civpatch-dev"
-		dockerfile = "Dockerfile.civpatch-dev"
-	}
-
-	if err := dockerClient.BuildImage(ctx, dockerfile, imageTag); err != nil {
-		fmt.Println("Error building image:", err)
-		os.Exit(1)
-	}
-
-	volumes := map[string]string{}
-	if develop {
-		volumes = map[string]string{
-			".": "/app",
+	if scrapeRun {
+		err = commands.ScrapeRun(ctx, *state, *gnis, *createPr, *develop, *withCi)
+		if err != nil {
+			return err
 		}
 	}
 
-	args := map[string]string{
-		"GITHUB_TOKEN": github_token,
+	if output != "" {
+		fmt.Println(output)
 	}
-
-	cmd := []string{"rake", fmt.Sprintf("pipeline:fetch[%s,%s,%t]", state, gnis, createPr)}
-
-	containerID, logs, logCancel, err := dockerClient.RunContainer(ctx,
-		imageTag,
-		requiredEnvVarsCI,
-		args,
-		cmd,
-		volumes)
-	if err != nil {
-		fmt.Println("Error running container:", err)
-		os.Exit(1)
-	}
-
-	defer logCancel()
-
-	fmt.Println("Container started:", containerID)
-
-	scanner := bufio.NewScanner(logs)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-
 	return nil
 }
 
 func main() {
+	ctx := context.Background()
 	args := os.Args[1:]
 	if len(args) == 0 {
 		fmt.Println("Usage: civpatch <command> [options]")
@@ -146,9 +61,17 @@ func main() {
 	switch args[0] {
 	case "scrape":
 		scrapeCommand.Parse(args[1:])
+		if err := scrapeCommands(ctx, *scrapePlan, *scrapeRun); err != nil {
+			handleError(err)
+		}
+	case "deploy":
+		commands.Deploy(ctx)
+	case "auth-clear":
+		commands.AuthClear()
 	}
+}
 
-	if scrapeCommand.Parsed() {
-		scrape(*state, *gnis, *createPr, *withCi, *develop)
-	}
+func handleError(err error) {
+	fmt.Println(err)
+	os.Exit(1)
 }
--- lib/services/github.rb
+++ lib/services/github.rb
@@ -8,11 +8,14 @@ module Services
 
     def initialize(develop: false)
       @client = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
-      @repo = "CivicPatch/open-data"
-      @local_repo = Git.open(PathHelper.project_path(""))
-      return unless develop
+      @repo = "https://#{ENV["GITHUB_USERNAME"]}:#{ENV["GITHUB_TOKEN"]}@github.com/CivicPatch/open-data.git"
+      @local_repo = develop ? Git.open(PathHelper.project_path("")) : Git.init
+      return if develop
 
       @local_repo.config("core.sparseCheckout", "true")
+      @local_repo.add_remote("origin", @repo)
+      @local_repo.fetch("origin")
+      @local_repo.checkout("main")
     end
 
     def pull_and_create_branch(municipality_context)
@@ -32,7 +35,7 @@ module Services
       state = municipality_context[:state]
       municipality_name = municipality_context[:municipality_entry]["name"]
 
-      @local_repo.add(all: true)
+      @local_repo.add(%w[data data_source])
       @local_repo.commit("Add municipal officials for #{municipality_name}, #{state}")
 
       pull_request_body = %(
@@ -41,7 +44,7 @@ module Services
       @client.create_pull_request(
         "CivicPatch/open-data",
         "main",
-        "head", # TODO: Add head
+        branch_name, # TODO: Add head
         "Municipality Officials: #{municipality_context[:municipality_entry]["name"]}, #{municipality_context[:state]}",
         pull_request_body
       )
