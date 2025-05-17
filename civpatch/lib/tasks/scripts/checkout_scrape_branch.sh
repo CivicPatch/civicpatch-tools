0d11d21fe86fb0f42ba335224d8c4b06aacdd8e4
michelle@shelltr.me <michelle@shelltr.me>
2025-05-17 15:59:27 -0700

    More changes

---
+++ civpatch/lib/tasks/scripts/checkout_scrape_branch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+STATE_ARG=$1
+GNIS_ARG=$2
+CREATE_PR_ARG=$3
+
+if [ -z "$STATE_ARG" ]; then
+  echo "Usage: $0 <state_code> <gnis_code>"
+  exit 1
+fi
+
+if [ -z "$GNIS_ARG" ]; then
+  echo "Usage: $0 <state_code> <gnis_code>"
+  exit 1
+fi
+
+echo "Calling rake pipeline:fetch[$STATE_ARG, $GNIS_ARG, $CREATE_PR_ARG]"
+
+REPO_URL="https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/CivicPatch/civicpatch-tools.git"
+
+cd /app
+git clone $REPO_URL .
+cd civicpatch-tools/civpatch
+
+local_run_id=$(uuidgen)
+branch_name="local-city-scrape-${STATE_ARG}-county-${GNIS_ARG}-${local_run_id}"
+echo "Checking out new branch: ${branch_name}"
+git checkout -b ${branch_name}
+
+
+
+
