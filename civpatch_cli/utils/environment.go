8ea488797b32403dca23255d900d65ca2ff7cf87
michelle@shelltr.me <michelle@shelltr.me>
2025-05-26 11:15:34 -0700

    Update cloudflare error messaging

--- civpatch_cli/utils/environment.go
+++ civpatch_cli/utils/environment.go
@@ -31,13 +31,16 @@ var (
 	}
 
 	requirementsText = map[string]string{
-		"GITHUB_USERNAME":         "GITHUB_USERNAME: Set up a GitHub account and create a username (for pipelines, use github.actor)",
-		"GITHUB_TOKEN":            "GITHUB_TOKEN: Set up a PAT or use secrets.GITHUB_TOKEN (for pipelines, use secrets.GITHUB_TOKEN)",
-		"OPENAI_TOKEN":            "OPENAI_TOKEN: Set up an OpenAI account and create an API key: https://platform.openai.com/api-keys",
-		"BRAVE_TOKEN":             "BRAVE_TOKEN: Set up a Brave Search account and create an API key: https://api-dashboard.search.brave.com/app/keys",
-		"GOOGLE_SEARCH_API_KEY":   "GOOGLE_SEARCH_API_KEY: Set up a project with Google Custom Search enabled and create an API key: https://developers.google.com/custom-search/v1/overview",
-		"GOOGLE_SEARCH_ENGINE_ID": "GOOGLE_SEARCH_ENGINE_ID: Set up a project with Google Custom Search enabled and get an Engine ID: https://cse.google.com/cse/all",
-		"GOOGLE_GEMINI_TOKEN":     "GOOGLE_GEMINI_TOKEN: Set up a project with Google Gemini enabled and create an API key: https://aistudio.google.com/apikey",
+		"GITHUB_USERNAME":             "GITHUB_USERNAME: Set up a GitHub account and create a username (for pipelines, use github.actor)",
+		"GITHUB_TOKEN":                "GITHUB_TOKEN: Set up a PAT or use secrets.GITHUB_TOKEN (for pipelines, use secrets.GITHUB_TOKEN)",
+		"OPENAI_TOKEN":                "OPENAI_TOKEN: Set up an OpenAI account and create an API key: https://platform.openai.com/api-keys",
+		"BRAVE_TOKEN":                 "BRAVE_TOKEN: Set up a Brave Search account and create an API key: https://api-dashboard.search.brave.com/app/keys",
+		"GOOGLE_SEARCH_API_KEY":       "GOOGLE_SEARCH_API_KEY: Set up a project with Google Custom Search enabled and create an API key: https://developers.google.com/custom-search/v1/overview",
+		"GOOGLE_SEARCH_ENGINE_ID":     "GOOGLE_SEARCH_ENGINE_ID: Set up a project with Google Custom Search enabled and get an Engine ID: https://cse.google.com/cse/all",
+		"GOOGLE_GEMINI_TOKEN":         "GOOGLE_GEMINI_TOKEN: Set up a project with Google Gemini enabled and create an API key: https://aistudio.google.com/apikey",
+		"CLOUDFLARE_R2_ENDPOINT":      "CLOUDFLARE_R2_ENDPOINT: Set up a Cloudflare account",
+		"CLOUDFLARE_R2_ACCESS_KEY_ID": "CLOUDFLARE_R2_ACCESS_KEY_ID: Set up a Cloudflare account",
+		"CLOUDFLARE_R2_SECRET_KEY":    "CLOUDFLARE_R2_SECRET_KEY: Set up a Cloudflare account",
 	}
 )
 
