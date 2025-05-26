9a1adceba7dd362f1a522c35d45d8a29f14b7e6f
michelle@shelltr.me <michelle@shelltr.me>
2025-05-26 13:19:50 -0700

    Remove quotes from PR

--- civpatch/lib/tasks/scripts/create_pull_request.sh
+++ civpatch/lib/tasks/scripts/create_pull_request.sh
@@ -18,8 +18,8 @@ BRANCH_NAME=$(git branch --show-current)
 
 PULL_REQUEST_DETAILS=$(rake "github_pipeline:pr_details[$STATE,$GEOID,$BRANCH_NAME]")
 
-PR_TITLE=$(printf "$PULL_REQUEST_DETAILS" | jq '.pr_title' )
-PR_BODY=$(printf "$PULL_REQUEST_DETAILS" | jq '.pr_body' )
+PR_TITLE=$(printf $PULL_REQUEST_DETAILS | jq '.pr_title' )
+PR_BODY=$(printf $PULL_REQUEST_DETAILS | jq '.pr_body' )
 
 if [[ -n $GITHUB_ENV ]]; then
   gh pr create --title "$PR_TITLE" --body "$PR_BODY" --label "$GITHUB_ENV" --base main
