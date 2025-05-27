60879019c3839960760ab154357d7de65b2658b9
michelle@shelltr.me <michelle@shelltr.me>
2025-05-27 10:36:00 -0700

    Update lebanon cdn images

--- civpatch/lib/core/cache_manager.rb
+++ civpatch/lib/core/cache_manager.rb
@@ -11,6 +11,7 @@ module Core
       cache_folders = Pathname.new(cache_dir).children.select(&:directory?).collect(&:to_s)
 
       cache_folders.each do |cache_folder|
+        next if cache_folder.include?("images")
         next if urls_to_keep.any? { |url| cache_folder.include?(url) }
 
         FileUtils.rm_rf(cache_folder)
