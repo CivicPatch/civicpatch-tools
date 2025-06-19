43e5aae4ea823c642815ed64913db5f3d2694861
michelle@shelltr.me <michelle@shelltr.me>
2025-06-19 11:29:05 -0700

    Keep cached files (only markdown)

--- civpatch/lib/core/cache_manager.rb
+++ civpatch/lib/core/cache_manager.rb
@@ -2,8 +2,8 @@
 
 module Core
   class CacheManager
-    def self.clean(state, geoid)
-      # urls_to_keep = source_urls.map { |source| Utils::UrlHelper.url_to_safe_folder_name(source) }
+    def self.clean(state, geoid, source_urls)
+      urls_to_keep = source_urls.map { |source| Utils::UrlHelper.url_to_safe_folder_name(source) }
 
       cache_dir = Core::PathHelper.get_city_cache_path(state, geoid)
       return unless Dir.exist?(cache_dir)
@@ -11,7 +11,14 @@ module Core
       cache_folders = Pathname.new(cache_dir).children.select(&:directory?).collect(&:to_s)
 
       cache_folders.each do |cache_folder|
-        # next if urls_to_keep.any? { |url| cache_folder.include?(url) }
+        if urls_to_keep.any? { |url| cache_folder.include?(url) }
+          # Remove everything except the markdown file
+          files_to_remove = Dir.glob(File.join(cache_folder, "*")).reject { |file| file.end_with?(".md") }
+          
+          FileUtils.rm_rf(files_to_remove)
+          next
+        end
+
         FileUtils.rm_rf(cache_folder)
       end
     end
