48ee08a0e79d4624727814e359d39253d4640047
michelle@shelltr.me <michelle@shelltr.me>
2025-07-31 21:41:53 -0700

    Split out config file, update tests

---
+++ civicpatch/src/utils/config_utils.py
@@ -0,0 +1,25 @@
+import os
+import yaml
+import utils.path_utils as path_utils
+
+def get_divisions():
+    """
+    Returns a list of divisions from the configuration file.
+    """
+    config_path = path_utils.get_config_path()
+    divisions_file_path = os.path.join(config_path, 'divisions.yaml')
+    with open(divisions_file_path, 'r') as config_file:
+        config_data = yaml.safe_load(config_file)
+    
+    return config_data.get('divisions', [])
+
+def get_government_types():
+    """
+    Returns a dictionary of government types from the configuration file.
+    """
+    config_path = path_utils.get_config_path()
+    government_types_file_path = os.path.join(config_path, 'government_types.yaml')
+    with open(government_types_file_path, 'r') as config_file:
+        config_data = yaml.safe_load(config_file)
+    
+    return config_data.get('government_types', {})
