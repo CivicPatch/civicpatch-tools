6a96f6fdc989306d2536dde373b0c4f972eeb5cc
michelle@shelltr.me <michelle@shelltr.me>
2025-08-25 18:34:45 -0700

    Add scaffolding for crudder

--- README.md
+++ README.md
@@ -1,105 +1,14 @@
-# OpenData
+# civicpatch-tools
 
-## Pipeline Setup
-* Join civicpatch-tools repo
-* Set up environment variables under environment: `env-<github-username>`
-  * Google Search
-  * Brave Search
-  * OpenAI
-  * Google Gemini
-* Actions -> Run "Municipal Officials - Scrape"
+Set of projects that support the collection of elected municipal officials across
+the United States.
 
-## Contribute
+To see the entire set of data, check the [open-data](https://github.com/CivicPatch/open-data/) repo.
 
-### Run Scrapes
+## Projects
 
-1. Download latest [release](https://github.com/CivicPatch/civicpatch-tools/releases)
-2. Set environment variables:
-  * OPENAI_TOKEN
-  * GOOGLE_GEMINI_TOKEN
-3. Find a GEOID to scrape under data_source/[state]/municipalities.json
-4. Run the command
-```sh
-./civpatch scrape -run -state wa -geoid 5335940
-```
-
-This will start a docker container, run the scrape, and when the scrapes are complete, output to the /output folder.
-
-####
-
-## Development
-### Requirements
-* Docker
-* [Go](https://go.dev/doc/install)
-* [Air](https://github.com/air-verse/air?tab=readme-ov-file#via-go-install-recommended)
-
-## Sources
-* All populations & municipalities are pulled primarily from US Census
-  * Population
-    * [CENSUS_POPULATION_API](https://api.census.gov/data/2020/dec/pl?get=P1_001N,NAME&for=place:*&in=state:43)
-  * Codes (fips & gnis codes)
-    * [CENSUS_MUNICIPALITIES_CODES](https://www2.census.gov/geo/docs/reference/codes2020/place/st53_wa_place2020.txt)
-* [https://docs.google.com/spreadsheets/d/1QtcMQV85HUxTayyk8mVO5Eqr8bWiNEdGc12RuoWuLpM/edit?gid=646812149#gid=646812149](List of state-level municipal officials)
-### Washington
-- **State local officials directory (unofficial)**: https://mrsc.org/mrsctools/officials-directory/city.aspx
-  - Contains city council members
-### Oregon
-- **State local officials directory (unofficial)**: https://www.orcities.org/resources/reference/city-directory
-### California
-- **State local officials directory (official SOS)**: 
-  - https://www.sos.ca.gov/administration/california-roster (2024 only)
-### Idaho (TBD)
-- **State local officials directory (unofficial)**:
-  - https://idahocities.org/store/ListProducts.aspx?catid=440197 (not scrapeable -- must be a member)
-### New Hampshire
-- Combo of
-  - [Community Profiles](https://www.nhes.nh.gov/elmi/products/cp/)
-    - TIL -- what are Selectmen???
-    - Gives you government type, website, general email, general phone #
-    - More data than below
-  - [List of municipalities](https://www.nheconomy.com/office-of-planning-and-development/what-we-do/state-data-center-(census-data)/municipalities,-counties-and-regions)
-    - Contains websites & counties, easier to scrape
-  - For officials -- do a google search
-### Michigan
-- For townships: https://michigantownships.org/find-a-township#aftersearch
-  - Contains city council members!
-  - This looks SUPER well maintained; haven't verified data
-- For cities/etc -- do a google search
-  
-### FIPS examples
-* https://www2.census.gov/geo/docs/reference/codes2020/place/st41_or_place2020.txt
-
-### Playwright Setup
-* npm install playwright
-* ./node_modules/.bin/playwright install
-
-### Services
-* Search Services
-  * Google Search - free tier only
-    * [Manage](https://console.cloud.google.com/apis/api/customsearch.googleapis.com)
-    * Free Tier - 100 req/day
-      * (should error out)
-    * Paid Tier - $5.00 per 1000 requests
-  * Brave - free tier only
-    * [Manage](https://api-dashboard.search.brave.com/app/dashboard)
-    * Free Tier - 2000 req/month
-* Google Gemini 2.5 Flash (used with scraping) - Paid Tier 1
-  * [Manage](https://console.cloud.google.com/apis/api/generativelanguage.googleapis.com/metrics)
-  * [Pricing](https://ai.google.dev/gemini-api/docs/pricing)
-  * Input: $0.15 Per Million
-  * Output: $0.60 Per Million
-* OpenAI API - GPT 4.1 Mini (used with scraping)
-  * [Pricing](https://platform.openai.com/docs/pricing)
-  * Input: $0.40 Per Million
-  * Output: $1.60 Per Million
-* Digital Ocean
-  * Host images under spaces
-
-### Links
-* https://editor.dicebear.com/
-
-### Misc Setup
-* Set up gitleaks
-  * Move [this](https://raw.githubusercontent.com/gitleaks/gitleaks/958f55ac629b5351bb7b9c80f00c10823fef64d3/scripts/pre-commit.py) to .git/hooks/pre-commit.py
-
-
+- [civicpatch](./civicpatch/README.md)
+  The main project. Scrapes websites for contact information on elected officials.
+  Scrape jobs are run on either GitHub Actions or by volunteers running scrapes on their own servers.
+- [crudder](./crudder/README.md)
+  Helper project that sits civicpatch volunteer servers & GitHub Actions.
--- crudder/src/main.py
+++ crudder/src/main.py
@@ -1,7 +1,35 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import os
+import datetime
+from fastapi import FastAPI, Request, Security, HTTPException, Depends
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.security import APIKeyCookie  # this is the part that puts the lock icon to the docs
+from fastapi_sso.sso.github import GithubSSO
+from fastapi_sso.sso.base import OpenID
+
+from jose import jwt  # pip install python-jose[cryptography]
+
+# Only purpose is to manage users, their API keys, and move data from 3rd party servers
+# to GitHub Actions.
+# Ref: https://github.com/tomasvotava/fastapi-sso/blob/master/docs/how-to-guides/use-with-fastapi-security.md
+
+GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+GITHUB_CALLBACK_URL = os.getenv("GITHUB_CALLBACK_URL", "http://127.0.0.1:8000/auth/github/callback")
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+if not all([GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, JWT_SECRET_KEY]):
+    missing_vars = [var for var, val in {
+        "GITHUB_CLIENT_ID": GITHUB_CLIENT_ID,
+        "GITHUB_CLIENT_SECRET": GITHUB_CLIENT_SECRET,
+        "JWT_SECRET_KEY": JWT_SECRET_KEY
+    }.items() if not val]
+    print(f"Missing environment variables: {', '.join(missing_vars)}")
+    raise ValueError("One or more required environment variables are not set.")
+
+github_sso = GithubSSO(client_id=GITHUB_CLIENT_ID, client_secret=GITHUB_CLIENT_SECRET, redirect_uri=GITHUB_CALLBACK_URL)
 
 app = FastAPI(
     title="Crudder API",
@@ -13,9 +41,97 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
+async def get_logged_user(cookie: str = Security(APIKeyCookie(name="token"))) -> OpenID:
+    """Get user's JWT stored in cookie 'token', parse it and return the user's OpenID."""
+    try:
+        claims = jwt.decode(cookie, key=JWT_SECRET_KEY, algorithms=["HS256"])
+        return OpenID(**claims["pld"])
+    except Exception as error:
+        raise HTTPException(status_code=401, detail="Invalid authentication credentials") from error
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+async def home(request: Request):
+    try:
+        user = await get_logged_user(request.cookies.get("token"))
+    except HTTPException:
+        user = None
+
     return templates.TemplateResponse(
-        request=request, name="item.html", context={"id": id}
+        request=request, name="index.html", context={"user": user}
     )
 
+@app.get("/api_keys", response_class=HTMLResponse, include_in_schema=False)
+async def api_keys_page(request: Request):
+    try:
+        user = await get_logged_user(request.cookies.get("token"))
+        # Fetch user's API keys from database
+
+        api_keys = [{
+            "id": "key1blahblah",
+            "suffix": "1234",
+            "name": "My First API Key",
+            "created_at": datetime.datetime.now(tz=datetime.timezone.utc).date()
+        }]
+
+        return templates.TemplateResponse(
+            request=request,
+            name="api_keys.html",
+            context={
+                "user": user,
+                "api_keys": api_keys
+            }
+        )
+    except HTTPException:
+        return RedirectResponse(url="/", status_code=302)
+
+@app.post("/api_keys", include_in_schema=False)
+async def create_api_key(request: Request):
+    """Create new API key"""
+    pass
+
+@app.delete("/api_keys/{key_id}", include_in_schema=False)
+async def revoke_api_key(request: Request, key_id: str):
+    """Revoke specific API key"""
+    pass
+
+@app.get("/auth/{provider}/login", include_in_schema=False)
+async def login(provider: str):
+    match provider:
+        case "github":
+            sso = github_sso
+        case _:
+            raise HTTPException(status_code=400, detail="Unsupported provider: {provider}")
+
+    async with sso:
+        return await sso.get_login_redirect()
+
+
+@app.get("/auth/logout", include_in_schema=False)
+async def logout():
+    """Forget the user's session."""
+    response = RedirectResponse(url="/")
+    response.delete_cookie(key="token")
+    return response
+
+
+@app.get("/auth/{provider}/callback", include_in_schema=False)
+async def login_callback(request: Request, provider: str):
+    match provider:
+        case "github":
+            sso = github_sso
+        case _:
+            raise HTTPException(status_code=400, detail="Unsupported provider")
+
+    """Process login and redirect the user to the protected endpoint."""
+    async with sso:
+        openid = await sso.verify_and_process(request)
+        if not openid:
+            raise HTTPException(status_code=401, detail="Authentication failed")
+    # Create a JWT with the user's OpenID
+    expiration = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1)
+    token = jwt.encode({"pld": openid.model_dump(), "exp": expiration, "sub": openid.id}, key=JWT_SECRET_KEY, algorithm="HS256")
+    response = RedirectResponse(url="/", status_code=302)
+    response.set_cookie(
+        key="token", value=token, expires=expiration
+    )  # This cookie will make sure /protected knows the user
+    return response
