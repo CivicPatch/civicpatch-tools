0f3dac9050b3703644b0053c9fd5cc88850db3c0
michelle@shelltr.me <michelle@shelltr.me>
2025-08-28 11:43:08 -0700

    update comment

--- crudder/src/auth.py
+++ crudder/src/auth.py
@@ -2,7 +2,6 @@ import hmac
 import hashlib
 
 def hash_string(string: str, hash_key: str) -> str:
-    """Hash an API key using HMAC-SHA256 and the database hash key."""
     return hmac.new(
         hash_key.encode(),
         string.encode(),
