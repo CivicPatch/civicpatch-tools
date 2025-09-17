acae08fba3aabf70fd86510b665d5a004efd3f3c
shelltr <michelle@civicpatch.org>
2025-09-17 16:42:33 -0700

    Switch to using bot token instead of repo default token for creating prs (workflow issue)

--- civicpatch/src/scripts/test_pipeline_step.py
+++ civicpatch/src/scripts/test_pipeline_step.py
@@ -8,6 +8,9 @@ def test_pipeline_step():
     state = "wa"
     geoid = "5363000"  # Seattle
     pipeline_status = PipelineStatus.MAYBE_SEND_TO_GITHUB
+    # Note: need to generate a unique request id
+    # if you are testing PR creation
+    request_id = "c3bcb6d3-a391-49b2-9f60-b51d5df69bea"
 
     pipeline = Pipeline()
     pipeline.set_state(pipeline_status)
@@ -17,11 +20,11 @@ def test_pipeline_step():
 
     with open(pipeline_context_path, "r") as f:
         context = json.load(f)
-        context["request_id"] = "test_request_id"
+        context["request_id"] = request_id
     with open(pipeline_context_path, "w") as f:
         json.dump(context, f, indent=2)
 
-    pipeline.run("test_request_id", state, geoid) 
+    pipeline.run(request_id, state, geoid) 
 
 if __name__ == "__main__":
     test_pipeline_step()
