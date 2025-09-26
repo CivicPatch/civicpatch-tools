a8a3868d331235d991a32e727299a5fe3c994de6
shelltr <michelle@civicpatch.org>
2025-09-26 11:06:48 -0700

    Fix crudder deploy and intake

--- civicpatch/src/scripts/test_pipeline_step.py
+++ civicpatch/src/scripts/test_pipeline_step.py
@@ -1,15 +1,16 @@
-from schemas import PipelineContext, PipelineStatus
+from schemas import PipelineContext, PipelineRequest, PipelineStatus
 from pipeline import Pipeline
 from utils.data_path_utils import get_pipeline_context_file_path
 import os
 import json
+import asyncio
 
-def test_pipeline_step():
+async def test_pipeline_step():
     jurisdiction_id = "ocd_jurisdiction/country:us/state:nc/place:greensboro"
     pipeline_status = PipelineStatus.MERGE_RECORDS_WITHIN_LLM
     # Note: need to generate a unique request id
     # if you are testing PR creation
-    request_id = "aaaaa"
+    request_id = "bbbbbbbbbbbbbb"
 
     pipeline = Pipeline()
     pipeline.set_state(pipeline_status)
@@ -23,7 +24,12 @@ def test_pipeline_step():
     with open(pipeline_context_path, "w") as f:
         json.dump(context, f, indent=2)
 
-    pipeline.run(request_id, jurisdiction_id)
+    pipeline_request = PipelineRequest(
+        jurisdiction_id=jurisdiction_id, 
+        name="Test", 
+        url="http://example.com"
+    )
+    await pipeline.run_async(request_id, pipeline_request)
 
 if __name__ == "__main__":
-    test_pipeline_step()
+    asyncio.run(test_pipeline_step())
