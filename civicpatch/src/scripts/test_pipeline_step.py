133fddbbb9c7bd51b0e9b64088d26d50d95526bc
shelltr <michelle@civicpatch.org>
2025-10-19 14:17:33 -0700

    Add start/stop functionality

--- civicpatch/src/scripts/test_pipeline_step.py
+++ civicpatch/src/scripts/test_pipeline_step.py
@@ -4,32 +4,23 @@ from utils.data_path_utils import get_pipeline_context_file_path
 import json
 import asyncio
 import uuid
+from pipeline_manager import PipelineManager
+
+pipeline_manager = PipelineManager()
 
 async def test_pipeline_step():
     #jurisdiction_id = "ocd_jurisdiction/country:us/state:nc/place:greensboro/government"
     jurisdiction_id = "ocd_jurisdiction/country:us/state:wa/place:seattle/government"
-    pipeline_status = PipelineStatus.MAYBE_SEND_TO_GITHUB
+    pipeline_status = PipelineStatus.MERGE_RECORDS_ACROSS_LLMS
     # Note: need to generate a unique request id
     # if you are testing PR creation
-    request_id = f"test_pipeline_{str(uuid.uuid4())}"
-    pipeline_request = PipelineRequest(
-        jurisdiction_id=jurisdiction_id, 
-        name="Test", 
-        url="http://example.com"
-    )
-
-    pipeline = Pipeline(request_id, pipeline_request)
-
-    # Update pipeline_context_path with request_id
-    pipeline_context_path = get_pipeline_context_file_path(jurisdiction_id)
-
-    with open(pipeline_context_path, "r") as f:
-        context = json.load(f)
-        context.request_id = request_id
-    with open(pipeline_context_path, "w") as f:
-        json.dump(context, f, indent=2)
- 
-    await pipeline.run_async(with_debug=True)
+    pipeline_manager.create_pipeline(
+        PipelineRequest(
+            jurisdiction_id=jurisdiction_id,
+            name="Seattle city",
+            url="https://seattle.gov/council"
+        ), debug_state=pipeline_status)
+    pipeline_manager.start_pipeline(jurisdiction_id)
 
 if __name__ == "__main__":
     asyncio.run(test_pipeline_step())
