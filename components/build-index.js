2af0fdb88fbd66e6837be6fc8cfe320027b67f77
michelle@shelltr.me <michelle@shelltr.me>
2025-11-17 12:54:29 -0800

    [components] Fix deploy script

--- components/build-index.js
+++ components/build-index.js
@@ -2,6 +2,7 @@ import fs from "fs";
 
 function build(prodMode) {
     const buildFolder = prodMode ? "dist" : "build"
+    fs.mkdirSync(buildFolder, { recursive: true });
     fs.copyFileSync("index.html", `${buildFolder}/index.html`)
     fs.cpSync("src/css", `${buildFolder}/css`, { recursive: true })
 }
