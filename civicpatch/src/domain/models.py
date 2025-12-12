a80c3bf485ed2e42f5443aa839efcca298cce2ce
michelle@shelltr.me <michelle@shelltr.me>
2025-12-12 10:10:05 -0800

    [civicpatch] wip - change models

--- civicpatch/src/domain/models.py
+++ civicpatch/src/domain/models.py
@@ -11,27 +11,27 @@ class Office(BaseModel):
 
 class Official(BaseModel):
     name: str
-    phone: Optional[str] = None 
-    email: Optional[str] = None 
-    url: Optional[str] = None 
-
-    image: Optional[str] = None
-    cdn_image: Optional[str] = None
-    sources: List[str]  
+    phones: List[str] = []
+    emails: List[str] = []
+    urls: List[str] = []
 
     office: Office = None
 
+    image: Optional[str] = None
+
     jurisdiction_id: str
+    cdn_image: Optional[str] = None
+    source_urls: List[str]  
     updated_at: str
 
 class Person(BaseModel):
     name: str
     # address: NOTE: not implemented, we are NOT collecting addresses
-    roles: List[str] 
-    divisions: List[str] 
-    phone: Optional[str] = None 
-    email: Optional[str] = None
-    url: Optional[str] = None
+    roles: List[str] = []
+    divisions: List[str] = []
+    phones: List[Optional[str]] = []
+    emails: Optional[str] = []
+    urls: Optional[str] = []
 
     start_date: Optional[str] = None
     end_date: Optional[str] = None
@@ -45,13 +45,10 @@ class Person(BaseModel):
 def person_to_official(person: Person) -> Official:
     return Official(
         name=person.name,
-        image=person.image,
-        jurisdiction_id=person.jurisdiction_id,
-        cdn_image=person.cdn_image,
 
-        phone=person.phone,
-        email=person.email,
-        url=person.url,
+        phones=person.phones,
+        emails=person.emails,
+        urls=person.urls,
 
         office=Office(
             name=" - ".join(person.roles),
@@ -59,7 +56,11 @@ def person_to_official(person: Person) -> Official:
             start_date=person.start_date,
             end_date=person.end_date,
         ),
+        
+        image=person.image,
 
+        jurisdiction_id=person.jurisdiction_id,
+        cdn_image=person.cdn_image,
         source_urls=person.source_urls,
         updated_at=person.updated_at,
     )
