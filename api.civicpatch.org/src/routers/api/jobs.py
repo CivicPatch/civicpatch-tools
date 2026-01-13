2b77e320736f7348f5431f985fc537cb61f240e8
michelle@shelltr.me <michelle@shelltr.me>
2026-01-12 19:05:52 -0800

    Update sync service; add pipeline api

--- api.civicpatch.org/src/routers/api/jobs.py
+++ api.civicpatch.org/src/routers/api/jobs.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter
+from typing import Optional
+from github_service import trigger_github_people_scrape_workflow
+import shared.utils.id_utils
+
 
 def get_router(api_key_header):
     router = APIRouter()
@@ -22,9 +26,34 @@ def get_router(api_key_header):
         # Implementation to get job status
         return {"request_id": request_id, "status": "pending"}
 
+    @router.post(
+        "/people",
+        summary="Trigger scrape people job",
+        description="Trigger a new scrape people job.",
+    )
+    async def trigger_people_job(
+        jurisdiction_ocdid: str, 
+        name: Optional[str] = None,
+        url: Optional[str] = None
+    ):
+        # Implementation to trigger a scrape people job
+        try:
+            request_id = shared.utils.id_utils.make_request_id()
+            response = trigger_github_people_scrape_workflow(
+                request_id=request_id,
+                jurisdiction_ocdid=jurisdiction_ocdid,
+                name=name,
+                url=url
+            )
+        except Exception as e:
+            print("Error triggering GitHub workflow:", e, response)
+            return {"status": "error"}, 500
+
+        return {"request_id": request_id, "status": "started"}
+
     @router.delete(
-        "/people/{request_id}/stop",
-        summary="Stop a job",
+        "/people/{request_id}",
+        summary="Cancel a job",
         description="Stop a specific job by its request ID.",
     )
     async def stop_job(request_id: str):
@@ -39,14 +68,5 @@ def get_router(api_key_header):
     async def get_job_logs(request_id: str):
         # Implementation to get job logs
         return {"request_id": request_id, "logs": []}
-
-    @router.post(
-        "/people",
-        summary="Trigger pipeline job",
-        description="Trigger a new pipeline job.",
-    )
-    async def trigger_pipeline_job():
-        # Implementation to trigger a pipeline job
-        return {"request_id": "new_request_id", "status": "started"}
 
     return router
