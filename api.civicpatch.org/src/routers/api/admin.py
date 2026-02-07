ae2007f76f095a32bf78603ca4871d0cf1276088
michelle@shelltr.me <michelle@shelltr.me>
2026-02-07 11:31:38 -0800

    [api] Split od sync endpoints

--- api.civicpatch.org/src/routers/api/admin.py
+++ api.civicpatch.org/src/routers/api/admin.py
@@ -11,11 +11,29 @@ def get_router(api_key_header, pool) -> APIRouter:
     router = APIRouter()
 
     @router.post("/od_sync", include_in_schema=False)
+    async def bulk_sync_endpoint(
+        background_tasks: BackgroundTasks,
+    ):
+        background_tasks.add_task(services.github_sync_service.bulk_sync)
+
+        return {"status": "running"}
+    
+    
+    @router.post("/od_sync_jurisdictions", include_in_schema=False)
+    async def sync_jurisdictions_endpoint(
+        request: OdSyncRequestSchema,
+        background_tasks: BackgroundTasks,
+    ):
+        background_tasks.add_task(services.github_sync_service.sync_jurisdictions_by_ocdids, request.jurisdiction_ocdids)
+
+        return {"status": "running"}
+    
+    @router.post("/od_sync_people", include_in_schema=False)
     async def sync_people_endpoint(
         request: OdSyncRequestSchema,
         background_tasks: BackgroundTasks,
     ):
-        background_tasks.add_task(services.github_sync_service.sync_data, request.jurisdiction_ocdids)
+        background_tasks.add_task(services.github_sync_service.sync_people_by_ocdids, request.jurisdiction_ocdids)
 
         return {"status": "running"}
     
