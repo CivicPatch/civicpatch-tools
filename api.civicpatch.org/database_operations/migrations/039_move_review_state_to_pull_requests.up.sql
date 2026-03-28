c65ef7f45cf73f9aa21330606f91e5f5764004ea
shelltr <michelle@civicpatch.org>
2026-03-27 18:20:53 -0700

    Fix migrations 35 & 39 - type cast

--- api.civicpatch.org/database_operations/migrations/039_move_review_state_to_pull_requests.up.sql
+++ api.civicpatch.org/database_operations/migrations/039_move_review_state_to_pull_requests.up.sql
@@ -5,7 +5,7 @@ ALTER TABLE pull_requests ADD COLUMN IF NOT EXISTS review_state TEXT;
 UPDATE pull_requests pr
 SET review_state = j.pull_request_review_state
 FROM jobs j
-WHERE j.request_id = pr.request_id;
+WHERE j.request_id::uuid = pr.request_id;
 
 ALTER TABLE jobs RENAME COLUMN pull_request_review_state TO pull_request_review_state_to_delete;
 
