9a65d1c4b3bbc26dd90800b96c82b7c324f61a3a
shelltr <michelle@civicpatch.org>
2026-04-23 13:18:12 -0700

    Remove unused components package and deploy workflow

--- components/src/index.js
+++
@@ -1,9 +0,0 @@
-import './api-config.js';
-import './hooks/use-api.js';
-// import './components/map.js';
-import './components/example.js';
-import './components/demo/demo-api-config.js';
-import './components/demo/demo-dashboard.js';
-
-export { apiConfig } from './api-config.js';
-export { useApi } from './hooks/use-api.js';
